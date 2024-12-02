760b6fef53e82777ae5a919ccf8b31a7507fe3fc
Javed Akhtar <conn.javed@gmail.com>
2024-12-02 10:10:59 +0530

    file updated

--- src/App.jsx
+++ src/App.jsx
@@ -45,14 +45,14 @@ function App() {
         <div className="text-sm text-center">Markdown Live Preview</div>
         <div className="flex justify-end items-center mr-5"><a className="hover:scale-105 transition-all" href="https://github.com/javed-ak/Markdown-Previewer.git" target="_blank"><FaGithub size={18}/></a></div>
       </div>
-      <div className="grid grid-col-1 lg:grid-cols-2 gap-2 h-full">
-        <div className="bg-zinc-50 shadow-lg h-screen lg:h-full">
+      <div className="grid grid-col-1 lg:grid-cols-2 gap-2 h-screen">
+        <div className="bg-zinc-50 shadow-lg h-screen">
           <div className="p-1 bg-green-600 text-slate-50 font-bold text-center shadow-lg sticky top-0">Editor</div>
           <div className="h-full overflow-y-auto">
             <textarea className="h-full w-full bg-zinc-50 focus:outline-none p-2" name="editor" id="editor" onChange={(e)=>{setText(e.target.value)}} value={text}></textarea>
           </div>
         </div>
-        <div className="bg-zinc-5 shadow-lg bg-zinc-50 h-full">
+        <div className="bg-zinc-5 shadow-lg bg-zinc-50 h-screen">
           <div className="p-1 bg-green-600 text-slate-50 font-bold text-center shadow-lg sticky top-0">Previewer</div>
           <div className="h-full overflow-y-auto">
             <div id="preview" className="prose p-5 h-full" dangerouslySetInnerHTML={{__html: marked(text)}}></div>
